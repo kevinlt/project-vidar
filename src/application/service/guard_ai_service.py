2a3fb5d43fdc98a0d8edab099c4cd0142116adff
kevinletexier <kevin.le.texier@gmail.com>
2025-11-22 21:44:22 +0100

    feat: extend GuardAiService with patrol position updates and index handling
    - Make `GuardAiService` inherit from `MovementService` for shared movement logic.
    - Add `update_guard_position` method to handle patrol point updates and index progression based on target reach.
    - Improve test coverage with scenarios for patrol index updates and map integration.

--- src/application/service/guard_ai_service.py
+++ src/application/service/guard_ai_service.py
@@ -1,15 +1,27 @@
+from src.application.service.movement_service import MovementService
 from src.domain.model.guard import Guard
+from src.domain.model.map import Map
 from src.domain.model.position import Position
 from src.domain.port.out.collision_service_port import CollisionServicePort
 
 
-class GuardAiService:
+class GuardAiService(MovementService):
 
     collision_service: CollisionServicePort
 
     def __init__(self, collision_service: CollisionServicePort):
-        self.collision_service = collision_service
+        super().__init__(collision_service)
 
-    def has_reach_target(self, position:Position, target: Position, patrol_radius: float):
+    def update_guard_position(self, guard: Guard, map_obj: Map) -> Position:
+        super().update_player_position(guard, map_obj, guard.patrol_points[guard.current_patrol_index])
+        patrol_point_reached = self.has_reach_target(guard.position, guard.patrol_points[guard.current_patrol_index], guard.patrol_radius)
+        if patrol_point_reached:
+            guard.current_patrol_index = self.update_patrol_index(guard)
+        return guard.patrol_points[guard.current_patrol_index]
+
+    def update_patrol_index(self, guard: Guard) -> int:
+        return (guard.current_patrol_index + 1) % len(guard.patrol_points)
+
+    def has_reach_target(self, position:Position, target: Position, patrol_radius: float) -> bool:
         return position.distance_to(target) < patrol_radius
 
--- tests/unit/application/service/test_guard_ai_service.py
+++ tests/unit/application/service/test_guard_ai_service.py
@@ -1,6 +1,7 @@
 from src.adapter.out.collision_service_impl import CollisionServiceImpl
 from src.application.service.guard_ai_service import GuardAiService
 from src.domain.model.guard import Guard
+from src.domain.model.map import Map
 from src.domain.model.position import Position
 
 
@@ -13,7 +14,17 @@ class TestGuardAiService:
 
     def test_guard_ai_service_reaches_target_position(self):
         guard = Guard()
+        guard.patrol_points = [Position(10., 10.), Position(11., 11.)]
         guard.position = Position(9.6, 10.)
-        target = Position(10., 10.)
         guard_ai_service = GuardAiService(CollisionServiceImpl())
-        assert guard_ai_service.has_reach_target(guard.position, target, guard.patrol_radius) == True
+        assert guard_ai_service.has_reach_target(guard.position, guard.patrol_points[guard.current_patrol_index], guard.patrol_radius) == True
+
+    def test_guard_current_patrol_index_is_next_when_target_is_reached(self):
+        guard = Guard()
+        guard.patrol_points = [Position(10., 10.), Position(11., 11.)]
+        guard.current_patrol_index = 0
+        guard.position = Position(9.6, 10.)
+        guard_ai_service = GuardAiService(CollisionServiceImpl())
+        map_obj = Map(10, 10)
+        guard_ai_service.update_guard_position(guard, map_obj)
+        assert guard.current_patrol_index == 1
